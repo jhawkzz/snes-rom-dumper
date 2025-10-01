749c54e9f56fead1db268d0fd01250730f26b95b
jhawkzz <jeredmcferron@gmail.com>
2025-09-30 20:35:55 -0700

    transfer

--- copy.bat
+++ copy.bat
@@ -1,1 +1,3 @@
-scp -r ./ jered@192.168.50.148:~/git/snes-rom-dumper/
+scp ./* jered@192.168.50.148:~/git/snes-rom-dumper/
+@echo off
+REM for recursive: scp -r ./ jered@192.168.50.148:~/git/snes-rom-dumper/
